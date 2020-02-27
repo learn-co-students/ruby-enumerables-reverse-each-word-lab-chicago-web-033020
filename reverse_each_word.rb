ee5e32f26988b16fb140bdb139f340529888c9d0
Shau Yin Luk <ivan.luk@icloud.com>
2020-02-27 19:43:21 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,8 @@ def reverse_each_word (input)
   output_array = []
   input_array = []
   input_array = input.split(" ")
-  input_array.collect {|word| output_array << word.reverse}
-  binding.pry
+  output_array = input_array.collect {|word| word.reverse}
+  # binding.pry
   output = output_array.join(" ")
   output  
 end
