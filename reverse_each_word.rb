e148360c7526259d72af56ec8b44dbfc5b9518ee
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-23 09:29:29 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,7 @@ array = array.split(" ")
 new_array = array.collect{|word| word.reverse}
 new_array.join(' ')
 end
+
+#def reverse_each_word(array)
+#  array.split(" ").collect {|x| x.reverse!}.join(" ")
+#end
