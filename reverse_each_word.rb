912c28d4bb5d5e6644ae292f3b0db70caaab8fd0
Claudiu Sarb <sarb.claudiu391@gmail.com>
2020-03-27 04:07:05 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(string)
+  string.split(" ").map{|element| element.reverse}.join(" ")
+end
+
+def reverse_each_word(string)
+  string.split(" ").collect{|element| element.reverse}.join(" ")
+end
